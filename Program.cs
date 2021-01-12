32cb5c0c70c86e8b69b754bfc2aa26228acd46f5
Josh Mann <jlmann72@gmail.com>
2021-01-12 10:04:49 -0500

    Playing around with old content

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace OurDotnetApp2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            string name = "Josh";
+            var rightNow = DateTime.Now;
+            Console.WriteLine($"Hello, {name}. Welcome to C#");
+            Console.WriteLine($"Your name is {name.Length} letters long.");
+            Console.WriteLine($"It's now {rightNow}.");
+        }
     }
-  }
 }
